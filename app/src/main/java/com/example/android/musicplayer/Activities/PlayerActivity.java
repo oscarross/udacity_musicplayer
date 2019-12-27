f4792dd62b760a4bd74ebd21bb16e9191c072374
Oskar Rosolowski <oskar.rosolowski@stepstone.com>
2019-12-27 12:11:36 +0100

    Setup player logic

--- app/src/main/java/com/example/android/musicplayer/Activities/PlayerActivity.java
+++ app/src/main/java/com/example/android/musicplayer/Activities/PlayerActivity.java
@@ -2,10 +2,14 @@ package com.example.android.musicplayer.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.musicplayer.Data.Song;
 import com.example.android.musicplayer.R;
@@ -19,6 +23,7 @@ public class PlayerActivity extends AppCompatActivity {
 
     private ArrayList<Song> songs = new ArrayList<>();
     private int position = -1;
+    private boolean isPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +42,37 @@ public class PlayerActivity extends AppCompatActivity {
         if (position >= 0) {
             Song currentSong = songs.get(position);
             setupWithSong(currentSong);
+            setupButtons();
         }
+    }
+
+    private void setupButtons() {
+        ImageButton backButton = findViewById(R.id.player_back_button);
+        backButton.setOnClickListener(new View.OnClickListener()   {
+            public void onClick(View v)  {
+                Song song = previousSong();
+                setupWithSong(song);
+            }
+        });
+
+        ImageButton nextButton = findViewById(R.id.player_next_button);
+        nextButton.setOnClickListener(new View.OnClickListener()   {
+            public void onClick(View v)  {
+                Song song = nextSong();
+                setupWithSong(song);
+            }
+        });
+
+        ImageButton playButton = findViewById(R.id.player_play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)  {
+                if (isPlaying) {
+                    stopPlaying();
+                } else {
+                    startPlaying();
+                }
+            }
+        });
     }
 
     private void setupWithSong(Song song) {
@@ -49,5 +84,55 @@ public class PlayerActivity extends AppCompatActivity {
 
         TextView detail = findViewById(R.id.player_song_detail);
         detail.setText(song.getArtistName());
+    }
+
+    private void startPlaying() {
+        isPlaying = true;
+        ImageButton playButton = findViewById(R.id.player_play_button);
+        playButton.setBackgroundResource(R.drawable.ic_pause);
+        showToast(getString(R.string.start_message));
+    }
+
+    private void stopPlaying() {
+        isPlaying = false;
+        ImageButton playButton = findViewById(R.id.player_play_button);
+        playButton.setBackgroundResource(R.drawable.ic_play);
+        showToast(getString(R.string.stop_message));
+    }
+
+    private Song nextSong() {
+        if (position < 0) {
+            return null;
+        }
+
+        position++;
+
+        if (position >= songs.size()) {
+            position = position % songs.size();
+        }
+
+        return songs.get(position);
+    }
+
+    private Song previousSong() {
+        if (position < 0) {
+            return null;
+        }
+
+        position--;
+
+        if (position < 0) {
+            position = songs.size() - 1;
+        }
+
+        return songs.get(position);
+    }
+
+    private void showToast(String message) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
     }
 }
